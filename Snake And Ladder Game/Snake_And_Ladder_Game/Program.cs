b3f8954416ffe00da919f404ddf2a77fb637037c
Akshata72 <sableakshata880@gmail.com>
2022-03-09 22:27:50 +0530

    Added:Snake and Ladder game played with single player

---
+++ Snake And Ladder Game/Snake_And_Ladder_Game/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SnakeAndLadderGame
+{
+    class SnakeAndLadderGame
+    {
+
+        static void Main(string[] args)
+        {
+            int Start_Player_Position = 0;
+            Random random = new Random();
+            int DiceRoll = random.Next(0, 7);
+            Console.WriteLine("Welcome to Snake And Ladder Game...");
+            Console.WriteLine("Starting Position of Player = " + Start_Player_Position);
+            Console.WriteLine("DiceRoll = " + DiceRoll);
+        }
+
+
+    }
+
+
+
+
+
+
+
+}
